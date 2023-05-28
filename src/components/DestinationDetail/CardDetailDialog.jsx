5276f9e012be2e2ba2765c6bac8925a88f0a3858
Ricardo Moreno <ricardo.yvl@gmail.com>
2023-05-28 20:41:06 -0300

    Path

--- src/components/DestinationDetail/CardDetailDialog.jsx
+++ src/components/DestinationDetail/CardDetailDialog.jsx
@@ -101,7 +101,7 @@ const CardDetailDialog = ({ destination, isOpen, onClose }) => {
                       ${destination.price}
                     </p>
                     <Link
-                      to="/form"
+                      to="/travelPrueba/"
                       className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded-md"
                     >
                       Seguir Viendo
