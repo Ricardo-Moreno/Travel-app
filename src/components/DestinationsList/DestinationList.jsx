548e7f7e97fa8eeff90251decd326d35e892f99e
Ricardo Moreno <ricardo.yvl@gmail.com>
2023-06-04 00:54:20 -0300

    Form

--- src/components/DestinationsList/DestinationList.jsx
+++ src/components/DestinationsList/DestinationList.jsx
@@ -1,17 +1,12 @@
 import PropTypes from "prop-types";
 import DestinationCard from "../DestinationCard/DestinationCard";
-import DestinationFormCard from "../DestinationsFormCard/DestinationsFormCard";
 
 const DestinationsList = ({ destinations }) => {
   return (
     <div className="mt-1 space-y-0 lg:grid lg:grid-cols-5 lg:space-y-0">
       {destinations.map((item) => (
         <div key={item.id}>
-          {item.type === "destination" ? (
-            <DestinationCard destination={item} />
-          ) : (
-            <DestinationFormCard destinationForm={item} />
-          )}
+          <DestinationCard destination={item} />
         </div>
       ))}
     </div>
